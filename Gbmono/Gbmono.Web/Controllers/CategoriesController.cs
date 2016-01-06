9e2d6fee1456c06380747aeb76015a90a7cdcc63
AllenYinSH <429616790@qq.com>
2016-01-06 20:48:56 +0800

    check CC

---
+++ Gbmono/Gbmono.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Gbmono.Models;
+using Gbmono.Models.Infrastructure;
+
+namespace Gbmono.Web.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+        private readonly RepositoryManager _repositoryManager;
+
+        #region ctor
+
+        public CategoriesController() : this(new RepositoryManager())
+        {
+
+        }
+
+        public CategoriesController(RepositoryManager manager)
+        {
+            _repositoryManager = manager;
+        }
+        #endregion
+
+        public IEnumerable<Category> GetAll()
+        {
+            return _repositoryManager.CategoryRepository.Table.ToList();
+        }
+    }
+}
