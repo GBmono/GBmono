0556fd2df4655ed68867d303d17b316a93878cd0
AllenYinSH <429616790@qq.com>
2016-01-07 22:08:05 +0800

    Init API For Commodity

--- Gbmono/Gbmono.Web/Controllers/CommodityController.cs
+++ Gbmono/Gbmono.Web/Controllers/CommodityController.cs
@@ -4,10 +4,39 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Gbmono.Models;
+using Gbmono.Models.Infrastructure;
 
 namespace Gbmono.Web.Controllers
 {
     public class CommodityController : ApiController
     {
+        private readonly RepositoryManager<Commodity> _repositoryManager;
+
+        #region ctor
+
+        public CommodityController() : this(new RepositoryManager<Commodity>())
+        {
+
+        }
+
+        public CommodityController(RepositoryManager<Commodity> manager)
+        {
+            _repositoryManager = manager;
+        }
+        #endregion
+
+        public IEnumerable<Commodity> GetCommodityListByConditions()
+        {
+            //Todo Search By Condition
+            return _repositoryManager.Repository.Table.ToList();
+        }
+
+
+        public Commodity GetCommodityDetail(int id)
+        {
+            return _repositoryManager.Repository.Get(id);
+        }
+
     }
 }
