4fcf71cd043c3e55c560c73863893a9ab1807ba7
Justin Li <blurlzy@hotmail.com>
2016-01-08 23:23:45 +1100

    created new sample to call web api then fetch the data

--- Gbmono/Gbmono.AngularJS/app/controllers/home/homeController.js
+++ Gbmono/Gbmono.AngularJS/app/controllers/home/homeController.js
@@ -3,13 +3,35 @@
 */
 (function (module) {
     // inject the controller params
-    ctrl.$inject = ['$scope'];
+    ctrl.$inject = ['$scope', 'productDataFactory'];
 
     // create controller
     module.controller('homeController', ctrl);
 
     // controller body
-    function ctrl($scope) {
+    function ctrl($scope, productDataFactory) {
+        // product list
+        $scope.products = [];
 
+        // call page init function
+        init();
+
+        // page init method
+        // 当该view被初始化时 需要执行的功能
+        function init() {
+            loadProducts();
+        }
+
+        // get products
+        function loadProducts() {
+            // call web api
+            productDataFactory.getProductList()
+                .success(function (data) {
+                    // success callback
+                    // retreive the data into local array
+                    // $scope.products can be accessed from the view
+                    $scope.products = data;
+                });
+        }
     }
 })(angular.module('gbmono'));
--- Gbmono/Gbmono.AngularJS/app/services/dataFactory.js
+++ Gbmono/Gbmono.AngularJS/app/services/dataFactory.js
@@ -1,4 +1,7 @@
-﻿(function (module) {
+﻿/*
+    category data factory
+*/
+(function (module) {
     // inject params
     factory.$inject = ['$http'];
 
@@ -16,6 +19,33 @@
             create: create,
             update: update,
             del: del
+        }
+    }
+
+})(angular.module('gbmono'));
+
+/*
+    product data factory
+*/
+(function (module) {
+    // inject params
+    factory.$inject = ['$http'];
+
+    // create instance
+    module.factory('productDataFactory', factory);
+
+    // factory implement
+    function factory($http) {
+        // api url prefix
+        var url = API_URL_ROOT + '/';
+
+        // return data factory with CRUD calls
+        return {
+            getProductList: getProductList
+        };
+
+        function getProductList() {
+            return $http.get(url);
         }
     }
 
