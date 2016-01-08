0f1f68b8a559235d23fedd9411a4efb9f4c12c2c
Justin Li <blurlzy@hotmail.com>
2016-01-08 22:22:41 +1100

    renamed commodity to product, updated the properties

---
+++ Gbmono/Gbmono.Models/Models/Product.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gbmono.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        // category 商品目录 根据当前目录id可获得上级目录
+        public int CategoryId { get; set; }
+
+        // 品牌商
+        public int ManufacturerId { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+
+        // 促销 code
+        public string PromotionCode { get; set; }
+
+        // 优惠券 code
+        public string CuponCode { get; set; }
+
+        // topic id
+        public int TopicId { get; set; }
+
+        // 名称
+        public string PrimaryName { get; set; }
+        // 次要名称
+        public string SecondaryName { get; set; }
+
+        // 商品气味 比如有些商品 含薄荷味
+        public string Flavor { get; set; }
+
+        // 容量
+        public double Content { get; set; }
+
+        // 条形码
+        public string BarCode { get; set; }
+
+        // 常规价格
+        public double Price { get; set; }
+
+        // discount??
+        public double Discount { get; set; }
+
+        // 商品描述
+        public string Description { get; set; }
+
+        // 使用说明
+        public string Instruction { get; set; }
+
+        // 商品图片
+        public ICollection<ProductImage> Images { get; set; }
+
+        // 零售店列表
+        public ICollection<RetailShop> RetailShops { get; set; }
+
+        // 网店列表
+        public ICollection<WebShop> WebShops { get; set; }
+
+
+        // 入库日期
+        public DateTime CreatedDate { get; set; }
+        
+        // 修改日期
+        public DateTime UpdatedDate { get; set; }
+
+        // 商品激活 (上货架日期)
+        public DateTime ActivationDate { get; set; }
+        
+        // 商品下货架日期
+        public DateTime ExpiryDate { get; set; }
+     
+        // todo:
+        // 季节推荐
+        // 关联商品
+    }
+}
---
+++ Gbmono/Gbmono.Models/Models/ProductImage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Gbmono.Models
+{
+    public class ProductImage
+    {
+        public int ProductImageId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public bool IsPrimary { get; set; }
+
+        public bool IsThumbnail { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
