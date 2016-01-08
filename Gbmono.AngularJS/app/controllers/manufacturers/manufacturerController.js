6809d6d302e74d4cc813684d0aa18ce15f1f7f98
Justin Li <blurlzy@hotmail.com>
2016-01-08 21:35:19 +1100

    add new folders for shops and manufacturers

---
+++ Gbmono.AngularJS/app/controllers/manufacturers/manufacturerController.js
@@ -0,0 +1,16 @@
+﻿/*
+   品牌商默认页 controller
+*/
+(function (module) {
+    // inject the controller params
+    ctrl.$inject = ['$scope', '$routeParams'];
+
+    // create controller
+    module.controller('manufacturerController', ctrl);
+
+    // controller body
+    function ctrl($scope, $routeParams) {
+        // get route param id from url
+        var id = $routeParams.id ? parseInt($routeParams.id) : 0;
+    }
+})(angular.module('gbmono'));
