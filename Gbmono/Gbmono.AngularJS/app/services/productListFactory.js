4150725041cd649907de9fcc2c9d0676c8814f96
Justin Li <blurlzy@hotmail.com>
2016-01-09 21:47:30 +1100

    new data factory sample

--- Gbmono/Gbmono.AngularJS/app/services/productListFactory.js
+++ Gbmono/Gbmono.AngularJS/app/services/productListFactory.js
@@ -25,6 +25,11 @@
             return $http.get(uri);
         }
 
+        // 注释：api_method 可删除 Route可直接放在http method里面 代码跟为简洁
+        //function getProductList() {
+        //    return $http.get(gbmono.api_site_prefix.product_api_url + '/GetProductList');
+        //}
+
         // return data factory with CRUD calls
         return {
             getProductList: getProductList
