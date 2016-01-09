a57eac5ddcf4f8bc5a69db4a8042449a82740118
AllenYinSH <429616790@qq.com>
2016-01-09 19:45:45 +0800

    product image

--- Gbmono/Gbmono.AngularJS/app/services/productListFactory.js
+++ Gbmono/Gbmono.AngularJS/app/services/productListFactory.js
@@ -3,11 +3,6 @@
 */
 (function (module) {
 
-    api_method = {
-        mGetProductList: "GetProductList"
-    }
-
-
     // inject params
     factory.$inject = ['$http'];
 
@@ -17,18 +12,10 @@
 
     // factory implement
     function factory($http) {
-        // api url prefix
-        var url = gbmono.api_site_prefix.product_api_url + '/';
 
         function getProductList() {
-            var uri = url + api_method.mGetProductList;
-            return $http.get(uri);
+            return $http.get(gbmono.api_site_prefix.product_api_url + '/GetProductList');
         }
-
-        // 注释：api_method 可删除 Route可直接放在http method里面 代码跟为简洁
-        //function getProductList() {
-        //    return $http.get(gbmono.api_site_prefix.product_api_url + '/GetProductList');
-        //}
 
         // return data factory with CRUD calls
         return {
