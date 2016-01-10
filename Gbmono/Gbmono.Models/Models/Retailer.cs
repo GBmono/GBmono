b7d578f070269b19e39481809ddb23a3cc0451af
Justin Li <blurlzy@hotmail.com>
2016-01-10 19:17:14 +1100

    added Retailer class

---
+++ Gbmono/Gbmono.Models/Models/Retailer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Gbmono.Models.Models
+{
+    /// <summary>
+    /// 零售商
+    /// </summary>
+    public class Retailer
+    {
+        public int RetailerId { get; set; }
+
+        public string Name { get; set; }
+
+        public string LogoUrl { get; set; }
+
+        public bool Enabled { get; set; }
+
+        // type:用来标识改零售商属于境外 境内 跨境通？
+    }
+}
