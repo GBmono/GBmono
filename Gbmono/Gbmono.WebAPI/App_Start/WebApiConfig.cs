7851f580edb8cdcab9a64953920c0e1091165567
raphael-yao <imbaygb@live.cn>
2016-01-14 21:53:42 +0800

    add log4net

--- Gbmono/Gbmono.WebAPI/App_Start/WebApiConfig.cs
+++ Gbmono/Gbmono.WebAPI/App_Start/WebApiConfig.cs
@@ -13,6 +13,7 @@ namespace Gbmono.WebAPI
     {
         public static void Register(HttpConfiguration config)
         {
+            log4net.Config.XmlConfigurator.Configure();
             config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
 
             // Web API configuration and services
