1494936f5939cad97975e8272e157f850ae6dc0c
AllenYinSH <429616790@qq.com>
2016-01-14 21:14:45 +0800

    Add auto migration for DB context

--- Gbmono/Gbmono.Models/Migrations/Configuration.cs
+++ Gbmono/Gbmono.Models/Migrations/Configuration.cs
@@ -5,7 +5,7 @@ namespace Gbmono.Models.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Gbmono.Models.DataContext.GbmonoSqlContext>
+    public sealed class Configuration : DbMigrationsConfiguration<Gbmono.Models.DataContext.GbmonoSqlContext>
     {
         public Configuration()
         {
