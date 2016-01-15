8b4081e5eeaa776f9fbb29db56594e6ba52816da
Justin Li <blurlzy@hotmail.com>
2016-01-15 22:49:57 +1100

    add json settings

--- Gbmono/Gbmono.WebAPI/App_Start/WebApiConfig.cs
+++ Gbmono/Gbmono.WebAPI/App_Start/WebApiConfig.cs
@@ -10,6 +10,8 @@ using System.Web.Http.Cors;
 using Gbmono.Models.DataContext;
 using Gbmono.WebAPI.ExceptionHandling;
 using Microsoft.SqlServer.Server;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace Gbmono.WebAPI
 {
@@ -40,6 +42,22 @@ namespace Gbmono.WebAPI
                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // using camel casing for property names
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            // ignore reference loop handling
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+            // remove default XML hanlder
+            var matches = config.Formatters
+                               .Where(f => f.SupportedMediaTypes.Where(
+                                m => m.MediaType.ToString() == "application/xml" || m.MediaType.ToString() == "text/xml").Count() > 0)
+                               .ToList();
+
+            foreach (var match in matches)
+            {
+                config.Formatters.Remove(match);    // remove
+            }
 
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<GbmonoSqlContext, Gbmono.Models.Migrations.Configuration>());
         }
