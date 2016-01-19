aeb76fce0865636d5cf056a9df95f936a81685df
Justin Li <blurlzy@hotmail.com>
2016-01-19 21:39:31 +1100

    fixed head controller issue

--- Gbmono/Gbmono.AngularJS/app/layout.js
+++ Gbmono/Gbmono.AngularJS/app/layout.js
@@ -15,6 +15,93 @@
 })(angular.module('gbmono'));
 
 
+/*
+   header block controller
+*/
+(function (module) {
+    // inject the controller params
+    ctrl.$inject = ['$scope', 'categoryDataFactory', 'accountDataFactory'];
+
+    // create controller
+    module.controller('headerController', ctrl);
+    // controller body
+    function ctrl($scope, categoryDataFactory, accountDataFactory) {
+        // define scope variable here
+        // login model
+        $scope.loginData = {};
+        // register model
+        $scope.registerData = {};
+
+        // call page init function
+        init();
+
+        // page init method
+        // 当该view被初始化时 需要执行的功能
+        function init() {
+            loadCategory();
+        }
+
+        // get products
+        function loadCategory() {
+            categoryDataFactory.getAll()
+             .success(function (data) {
+                 // success callback
+                 // retreive the data into local array
+                 // $scope.products can be accessed from the view
+                 $scope.categories = data;
+             });
+        }
+
+        // put web api in seperate function
+        // login function
+        function login(userName, password) {
+            accountDataFactory.login(userName, password)
+                 .success(function (data) {
+                     //todo store in localstorage
+                 });
+        }
+
+        // register function
+        function register(model) {
+            accountDataFactory.register(model)
+                 .success(function (data) {
+                     //todo store in localstorage
+                 });
+        }
+
+        // event handlers
+        $scope.register = function () {
+            // use email as userName
+            $scope.registerData.userName = $scope.registerData.email;
+            // show model
+            console.log($scope.registerData)
+        }
+
+
+        $scope.login = function () {
+            console.log($scope.loginData);
+        }
+    }
+
+})(angular.module('gbmono'));
+
+/*
+   left menubar controller
+*/
+(function (module) {
+    // inject the controller params
+    ctrl.$inject = ['$scope'];
+
+    // create controller
+    module.controller('leftMenuBarController', ctrl);
+
+    // controller body
+    function ctrl($scope) {
+
+
+    }
+
+})(angular.module('gbmono'));
 
 
 /*
