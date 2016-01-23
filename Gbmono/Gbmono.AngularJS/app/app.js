8b0fa0bbbab8631a0b83556cae85d5b8446701f8
AllenYinSH <429616790@qq.com>
2016-01-23 23:36:36 +0800

    ad video

--- Gbmono/Gbmono.AngularJS/app/app.js
+++ Gbmono/Gbmono.AngularJS/app/app.js
@@ -10,7 +10,7 @@
 // define angularJs module
 (function () {
     // inject the parameters
-    config.$inject = ['$routeProvider', '$httpProvider'];
+    config.$inject = ['$routeProvider', '$httpProvider', '$sceDelegateProvider'];
 
 
     // config
@@ -19,7 +19,7 @@
 
     // module config
     // config route & http
-    function config($routeProvider, $httpProvider) {
+    function config($routeProvider, $httpProvider, $sceDelegateProvider) {
         // inject authentication interceptor
         // bearer token authentication
         // $httpProvider
@@ -77,6 +77,13 @@
                     controller: 'homeController',
                     caseInsensitiveMatch: true
                 });
+
+        $sceDelegateProvider.resourceUrlWhitelist([
+       // Allow same origin resource loads.
+       'self',
+       // Allow loading from our assets domain.  Notice the difference between * and **.
+       'http://player.youku.com/**']);
+
 
     }
 
